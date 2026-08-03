27d9434b715cb5464c2d3e87f934c8f666678543
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-08-03 15:54:41 +0200

    fix: add async/await to apiController functions

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -2,22 +2,22 @@ import { type Request, type Response } from "express";
 
 import { getAllPosts } from "../models/postModel.js";
 
-export function getRandomPost(_req: Request, res: Response) {
-  const posts = getAllPosts();
+export async function getRandomPost(_req: Request, res: Response) {
+  const posts = await getAllPosts();
   const randomIndex = Math.floor(Math.random() * posts.length);
   const randomPost = posts[randomIndex];
   res.json(randomPost);
 }
 
-export function getLatestPosts(_req: Request, res: Response) {
-  const posts = getAllPosts();
+export async function getLatestPosts(_req: Request, res: Response) {
+  const posts = await getAllPosts();
   const sortedPosts = posts.sort((a, b) => b.createdAt - a.createdAt);
   const latestPosts = sortedPosts.slice(0, 3);
   res.json(latestPosts);
 }
 
-export function getStats(_req: Request, res: Response) {
-  const posts = getAllPosts();
+export async function getStats(_req: Request, res: Response) {
+  const posts = await getAllPosts();
   const totalPosts = posts.length;
   const sortedPosts = posts.sort((a, b) => b.createdAt - a.createdAt);
   const newestPost = sortedPosts[0];
